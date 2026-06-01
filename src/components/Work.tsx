3645ee273fd10551242864526d75ef01c18369ff
Mayur Bichukale <meetmayur91@gmail.com>
2026-06-01 17:03:59 +0530

    Add SEO to Inobal project tools

--- src/components/Work.tsx
+++ src/components/Work.tsx
@@ -50,7 +50,7 @@ const Work = () => {
     {
       name: "Inobal",
       category: "International Business Consulting Firm",
-      tools: "Laravel, cPanel, PHP",
+      tools: "Laravel, cPanel, PHP, SEO",
       link: "https://inobal.com/",
       image: "/images/inobal.jpg",
       alt: "Inobal - International Business Consulting Firm",
